3ded74db62be407949f1bf43eb862c1354c97b7b
Artyom1222 <134495433+Artyom1222@users.noreply.github.com>
2025-05-25 13:09:18 +0300

    lab+server+app+task

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import app from './app';
+import { PORT } from './common/config';
+import logger from './common/logger';
+
+process.on('uncaughtException', (error: Error): void => {
+  logger.error('Uncaught Exception:', { error, stack: error.stack });
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason: unknown, promise: Promise<unknown>): void => {
+  logger.error('Unhandled Rejection:', { reason, promise });
+  process.exit(1);
+});
+
+app.listen(PORT, (): void => {
+  logger.info(`App is running on http://localhost:${PORT}`);
+});
